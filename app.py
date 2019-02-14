5911a5be83c1dbba82e7323c9a6f882a6069076c
Paulo Sérgio Amaral <paulo.security@gmail.com>
2019-02-14 16:49:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 # encoding=utf8
 
 from flask import Flask, jsonify, request, abort, make_response
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+CORS(app)
 
 import random
 import math
