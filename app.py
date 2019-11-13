d8c1f99e0189d21d16e9daccb7b5d04f11e775d8
Paulo Sérgio Amaral <paulo.security@gmail.com>
2019-11-13 10:00:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -120,8 +120,7 @@ def complaint_search():
     end = limit * page
 
     cursor.sort([
-        ('id', pymongo.DESCENDING)]
-    
+        ('id', pymongo.DESCENDING),
         ('anoassedio', pymongo.DESCENDING),
         ('datassedio', pymongo.DESCENDING)]
     
